ae6e8d36a3cb4b35295511d511928e090041227b
Steven Fawcett <steven.fawcett@me.com>
2025-08-19 23:14:22 +0100

    partially rework the update screen

--- PluginBase.cs
+++ PluginBase.cs
@@ -1,17 +1,23 @@
-﻿using System.Diagnostics;
-using APIPlugin.Properties;
+﻿using APIPlugin.Properties;
 using BroadcastPluginSDK.abstracts;
 using BroadcastPluginSDK.Classes;
+using BroadcastPluginSDK.Interfaces;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace APIPlugin;
 
 public class PluginBase : BroadcastPluginBase
 {
+    private const string PluginName = "APIPlugin";
+    private const string PluginDescription = "REST API Plugin.";
+    private const string Stanza = "API";
+
     public class PluginSettings
     {
         public GetCacheDataDelegate? GetCacheData;
@@ -55,20 +61,22 @@ public class PluginBase : BroadcastPluginBase
 
     private readonly IWebHost _webhost;
     private static readonly Image s_icon = Resources.green;
+    private readonly ILogger<IPlugin> _logger;
 
-    public PluginBase(IConfiguration configuration) : base(configuration, null, s_icon, "API plugin", "API", "REST API Provider")
+    public PluginBase(IConfiguration configuration , ILogger<IPlugin> logger ) :
+        base(configuration, null, s_icon, PluginName, Stanza, PluginDescription)
     {
-
-        _webhost = CreateWebHostBuilder( configuration.GetSection("API") );
+        _logger = logger;
+        _webhost = CreateWebHostBuilder( configuration.GetSection(Stanza) );
         _webhost.RunAsync();
-        Debug.WriteLine("API plugin Started");
+        _logger.LogInformation(PluginDescription);
     }
 
-
-    private static IWebHost CreateWebHostBuilder(IConfiguration configuration)
+        
+    private IWebHost CreateWebHostBuilder(IConfiguration configuration)
     {
         var address = $"http://{configuration["server"]}:{configuration["port"]}";
-        Debug.WriteLine($"Starting API server on {address}");
+        _logger.LogInformation("Starting API server on {0}" , address);
 
         return WebHost.CreateDefaultBuilder()
             .UseUrls(address)
