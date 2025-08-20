a01af9a4abea0762e45608052d93388f9f2f1982
Steven Fawcett <steven.fawcett@me.com>
2025-08-20 21:33:49 +0100

    tidy up

--- PluginBase.cs
+++ PluginBase.cs
@@ -15,6 +15,9 @@ namespace APIPlugin;
 public class PluginBase : BroadcastPluginBase 
 {
     private const string Stanza = "API";
+    private readonly IWebHost? _webhost;
+    private static readonly Image s_icon = Resources.green;
+    private readonly ILogger<IPlugin> _logger;
 
     public class PluginSettings
     {
@@ -26,22 +29,15 @@ public class PluginBase : BroadcastPluginBase
         public PluginSettings Current { get; set; } = new();
     }
 
-    public class Startup
+    public class Startup( PluginSettingsAccessor accessor ) 
     {
-        private readonly PluginSettingsAccessor _accessor;
-
-        public Startup(PluginSettingsAccessor accessor)
-        {
-            _accessor = accessor;
-        }
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddSingleton(_accessor); // Already registered in CreateWebHostBuilder
+            services.AddSingleton( accessor); // Already registered in CreateWebHostBuilder
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public static void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
@@ -51,17 +47,6 @@ public class PluginBase : BroadcastPluginBase
 
     #region IPlugin Members
 
-    private IWebHost? _webhost;
-
-    public override GetCacheDataDelegate? GetCacheData
-    {
-        get => _webhost.Services.GetRequiredService<PluginSettingsAccessor>().Current.GetCacheData;
-        set => _webhost.Services.GetRequiredService<PluginSettingsAccessor>().Current.GetCacheData = value;
-    }
-
-    private static readonly Image s_icon = Resources.green;
-    private readonly ILogger<IPlugin> _logger;
-
     public PluginBase(IConfiguration configuration , ILogger<IPlugin> logger ) :
         base(configuration, null, s_icon,  Stanza)
     {
@@ -69,10 +54,10 @@ public class PluginBase : BroadcastPluginBase
         
         _webhost = CreateWebHostBuilder(configuration.GetSection(Stanza));
 
-        Task.Run(() => InitializeWebHost(configuration));
+        Task.Run(() => InitializeWebHost());
     }
 
-    private void InitializeWebHost(IConfiguration configuration)
+    private void InitializeWebHost()
     {
         _webhost.Run();
     }
@@ -80,7 +65,7 @@ public class PluginBase : BroadcastPluginBase
     private IWebHost CreateWebHostBuilder(IConfiguration configuration)
     {
         var address = $"http://{configuration["server"]}:{configuration["port"]}";
-        _logger.LogInformation("Starting API server on {0}" , address);
+        _logger.LogInformation("Starting API server on {address}" , address);
 
         return WebHost.CreateDefaultBuilder()
             .UseUrls(address)
@@ -94,5 +79,18 @@ public class PluginBase : BroadcastPluginBase
             .Build();
     }
 
+    public override GetCacheDataDelegate? GetCacheData
+    {
+        get => _webhost?.Services.GetRequiredService<PluginSettingsAccessor>().Current.GetCacheData;
+        set
+        {
+            if (_webhost == null)
+            {
+                Debug.WriteLine("Web host is not initialized. Cannot set GetCacheData.");
+                return;
+            }
+            _webhost.Services.GetRequiredService<PluginSettingsAccessor>().Current.GetCacheData = value;
+        }
+    }
     #endregion
 }
